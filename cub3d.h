ee66acff803e40967490fa22c76a9b8425be53e7
Mouataz Saadidi <saadidimouataz@gmail.com>
2024-08-11 19:00:20 +0100

    close to finishing parsing

--- cub3d.h
+++ cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: m3ayz00 <m3ayz00@student.42.fr>            +#+  +:+       +#+        */
+/*   By: msaadidi <msaadidi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 17:30:47 by msaadidi          #+#    #+#             */
-/*   Updated: 2024/08/11 01:12:03 by m3ayz00          ###   ########.fr       */
+/*   Updated: 2024/08/11 17:31:24 by msaadidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -137,7 +137,7 @@ int	ft_strcmp(char *s1, char *s2);
 int get_rgb(int t, int r, int g, int b);
 t_lst	*lst_new(char *row);
 void	ft_lstadd_back(t_lst **lst, t_lst *new);
-t_lst	*ft_lst_before(t_lst **lst, t_lst *curr);
+t_lst	*ft_lst_before(t_lst *lst, t_lst *curr);
 void	ft_lst_remove(t_lst	**lst, t_lst *to_rem);
 int	ft_lstsize(t_lst *lst);
 void	ft_lstclear(t_lst **lst);
